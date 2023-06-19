6c9198a8313c35c960bbd5e55b35c0a8441261b5
yusuf abdulsamad <yusufabdulsamad93@gmail.com>
2023-06-19 16:57:59 +0100

    fix the timezone

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,13 @@
-const moment = require('moment')
-function formatMessage(username, text){
-    return {
-        username,
-        text,
-        time:moment().format('h:mm a')
-    }
+const moment = require('moment');
 
+function formatMessage(username, text) {
+  const time = moment().utcOffset('+01:00').format('h:mm A'); // Set Nigeria time zone (WAT) offset
+
+  return {
+    username,
+    text,
+    time: ` ${time}`,
+  };
 }
 
-module.exports = formatMessage
+module.exports = formatMessage;
